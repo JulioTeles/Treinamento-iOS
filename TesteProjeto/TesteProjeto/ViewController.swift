a5707139f63b7741003cecb98dbbab8571624a00
administrador <administrador@M72441.local>
2019-07-16 09:39:35 -0300

    Criando Projeto

---
+++ TesteProjeto/TesteProjeto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TesteProjeto
+//
+//  Created by administrador on 16/07/19.
+//  Copyright © 2019 administrador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
